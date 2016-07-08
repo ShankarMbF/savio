66f6cae968b9f905921055f33c745b2b357fbfa9
getsavio <savio@madebyfire.co.uk>
2016-07-08 18:33:27 +0530

    Started working on Impulse saving

--- Savio/ImpulseSavingView/SAImpulseSavingViewController.swift
+++ Savio/ImpulseSavingView/SAImpulseSavingViewController.swift
@@ -9,18 +9,67 @@
 import UIKit
 
 class SAImpulseSavingViewController: UIViewController {
-
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var scrlView: UIScrollView!
+    @IBOutlet weak var messagePopupImageView: UIImageView!
+    @IBOutlet weak var messagePopUpView: UIView!
+    @IBOutlet weak var circularView: UIView!
+    
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    public var circleSlider: CircleSlider! {
+        didSet {
+            self.circleSlider.tag = 0
+        }
+    }
     var wishListArray : Array<Dictionary<String,AnyObject>> = []
-
+    
+    
+    private var valueLabel: UILabel!
+    private var progressLabel: UILabel!
+    private var timer: NSTimer?
+    private var progressValue: Float = 0
+    private var sliderOptions: [CircleSliderOption] {
+        return [
+            .BarColor(UIColor(red: 234/255, green: 235/255, blue: 237/255, alpha: 1)),
+            .ThumbImage(UIImage (named: "slider-handle@2x.png")),
+            .TrackingColor(UIColor(red: 244/255, green: 176/255, blue: 58/255, alpha: 1)),
+            .BarWidth(20),
+            .StartAngle(-90),
+            .MaxValue(100),
+            .MinValue(0),
+            .ThumbWidth(30)
+        ]
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        self.buildCircleSlider()
         self.setUpView()
         
     }
     
-
+    private func buildCircleSlider() {
+        
+        self.circleSlider = CircleSlider(frame: CGRectMake(0, 0, circularView.frame.size.width, circularView.frame.size.height), options: self.sliderOptions)
+        self.circleSlider?.addTarget(self, action: Selector("valueChange:"), forControlEvents: .ValueChanged)
+        
+        self.valueLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        self.valueLabel.textAlignment = .Center
+        
+        self.valueLabel.center = CGPoint(x: CGRectGetWidth(self.circleSlider.bounds) * 0.5, y: CGRectGetHeight(self.circleSlider.bounds) * 0.5)
+        self.circleSlider.addSubview(self.valueLabel)
+        circularView.addSubview(circleSlider)
+    }
+    
+    func valueChange(sender: CircleSlider) {
+        
+        print(sender.value)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -28,10 +77,12 @@ class SAImpulseSavingViewController: UIViewController {
     
     func setUpView(){
         
+        scrlView.contentSize = CGSizeMake(0, contentView.frame.height)
+        
         self.navigationController?.navigationBarHidden = false
         self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
-        self.title = "Update saving plan"
+        self.title = "Add Extra funds"
         
         
         //set Navigation left button
@@ -78,11 +129,90 @@ class SAImpulseSavingViewController: UIViewController {
         rightBarButton.customView = btnName
         self.navigationItem.rightBarButtonItem = rightBarButton
         
+        
+        var customToolBar : UIToolbar?
+        customToolBar = UIToolbar(frame:CGRectMake(0,0,UIScreen.mainScreen().bounds.size.width,44))
+        let acceptButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action:Selector("doneBarButtonPressed"))
+        
+        customToolBar!.items = [acceptButton]
+        
+        
+        let leftView = UILabel()
+        leftView.frame = CGRectMake(10, 0, 17, 50)
+        leftView.text = "£"
+        leftView.font = UIFont(name: "GothamRounded-Medium", size: 34)
+     
+        leftView.textAlignment = NSTextAlignment.Right
+        self.priceTextField.leftView = leftView
+        
+        self.priceTextField.leftViewMode = .Always
+        
+        priceTextField.inputAccessoryView = customToolBar
+        priceTextField.layer.borderColor = UIColor.blackColor().CGColor
+      
+        
     }
     
+    //UITextfieldDelegate method
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool
+    {
+    
+        //If the UIScreen size is 480 move the View little bit up so the UITextField will not be hidden
+        if(UIScreen.mainScreen().bounds.size.height == 480)
+        {
+            //UIViewAnimation for moving screen little bit up
+            UIView.beginAnimations(nil, context: nil)
+            UIView.setAnimationDelegate(self)
+            UIView.setAnimationDuration(0.5)
+            UIView.setAnimationBeginsFromCurrentState(true)
+            view!.frame = CGRectMake(view!.frame.origin.x, (view!.frame.origin.y-30), view!.frame.size.width, view!.frame.size.height)
+            UIView.commitAnimations()
+        }
+        else if(UIScreen.mainScreen().bounds.size.height == 568)
+        {
+            //UIViewAnimation for moving screen little bit up
+            UIView.beginAnimations(nil, context: nil)
+            UIView.setAnimationDelegate(self)
+            UIView.setAnimationDuration(0.5)
+            UIView.setAnimationBeginsFromCurrentState(true)
+            view!.frame = CGRectMake(view!.frame.origin.x, (view!.frame.origin.y-60), view!.frame.size.width, view!.frame.size.height)
+            UIView.commitAnimations()
+            
+        }
+     
+        return true
+    }
+
+    func doneBarButtonPressed(){
+        priceTextField.resignFirstResponder()
+        circleSlider.value = (priceTextField.text! as NSString).floatValue
+        if(UIScreen.mainScreen().bounds.size.height == 480)
+        {
+            UIView.beginAnimations(nil, context: nil)
+            UIView.setAnimationDelegate(self)
+            UIView.setAnimationDuration(0.5)
+            UIView.setAnimationBeginsFromCurrentState(true)
+            view!.frame = CGRectMake(view!.frame.origin.x, (view!.frame.origin.y+100), view!.frame.size.width, view!.frame.size.height)
+            UIView.commitAnimations()
+        }
+        else if(UIScreen.mainScreen().bounds.size.height == 568)
+        {
+            //UIViewAnimation for moving screen little bit up
+            UIView.beginAnimations(nil, context: nil)
+            UIView.setAnimationDelegate(self)
+            UIView.setAnimationDuration(0.5)
+            UIView.setAnimationBeginsFromCurrentState(true)
+            view!.frame = CGRectMake(view!.frame.origin.x, (view!.frame.origin.y+150), view!.frame.size.width, view!.frame.size.height)
+            UIView.commitAnimations()
+            
+        }
+
+    }
     func menuButtonClicked(){
         NSNotificationCenter.defaultCenter().postNotificationName(kNotificationToggleMenuView, object: nil)
     }
+    
+    
     
     func heartBtnClicked(){
         
@@ -98,16 +228,18 @@ class SAImpulseSavingViewController: UIViewController {
         }
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addFundsButtonPressed(sender: AnyObject) {
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

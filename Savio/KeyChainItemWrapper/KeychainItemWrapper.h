a64e6300d36873d84f83b629fb8499963e4e9c8b
erabala <bala@madebyfire.com>
2017-02-16 18:21:03 +0530

    CleanUp master

--- Savio/KeyChainItemWrapper/KeychainItemWrapper.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface KeychainItemWrapper : NSObject
-
-+ (void)save:(NSString *)service data:(id)data;
-+ (id)load:(NSString *)service;
-+ (void)delete:(NSString *)service;
-
-@end
